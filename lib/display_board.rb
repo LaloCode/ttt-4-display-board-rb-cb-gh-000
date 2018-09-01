e1c740bd39f86e7005359e3e1789123ec95d1f4b
José Lerma <lalo_pichu22@hotmail.com>
2018-09-01 19:17:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(state)
+  puts " #{state[0]} | #{state[1]} | #{sate[2]} }"
+  puts " #{state[3]} | #{state[4]} | #{sate[5]} }"
+  puts " #{state[6]} | #{state[7]} | #{sate[8]} }"
+end
